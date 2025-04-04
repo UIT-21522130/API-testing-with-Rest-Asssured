c2be6665592b0e899c70e729f63ebc3092705500
UIT-21522130 <96097433+UIT-21522130@users.noreply.github.com>
2025-04-04 15:46:37 +0700

    co len: random data

--- APITestingRestAssured/src/test/java/day6/RandomData.java
+++ APITestingRestAssured/src/test/java/day6/RandomData.java
@@ -1,5 +1,29 @@
 package day6;
+import org.json.JSONObject;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import java.util.HashMap;
 
 public class RandomData {
-
+	public String name = "";
+	public String physics = "";
+	@BeforeClass
+	public void setup() {
+		RestAssured.baseURI = "http://localhost:3000";
+	}
+	@BeforeTest
+	public void prepareTestData() {
+		
+	}
+	@Test
+	public void testAddStudent() {
+		
+	}
 }
